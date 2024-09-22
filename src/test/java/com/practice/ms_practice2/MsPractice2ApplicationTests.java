03c5976df290c5890cbbd9afe2d7724bbfb9b755
elpyazy <y.elpyaz@gmail.com>
2024-09-21 20:07:55 -0400

    Initial Commit

---
+++ src/test/java/com/practice/ms_practice2/MsPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.ms_practice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPractice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
